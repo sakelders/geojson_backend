73e6fbfd6670d7dc1471f010ca5a1d8cec8c509a
Sander Kelders <sakelders@thegnome.nl>
2025-03-22 14:58:54 +0100

    Create Municipality model and load initial data

--- geojson_backend/settings.py
+++ geojson_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_extensions',
+    'rest_api',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'geojson_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'geodjango',
+        'USER': 'geodjango',
+        'PASSWORD': 'geodjango',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- rest_api/models.py
+++ rest_api/models.py
@@ -1,3 +1,5 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Municipality(models.Model):
+    name = models.CharField(max_length=255)
+    polygons = models.MultiPolygonField()
